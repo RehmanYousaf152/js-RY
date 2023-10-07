f39c424b0c0c4e14d3266537bfb4fad47761f2bb
RehmanYousaf152 <147226228+RehmanYousaf152@users.noreply.github.com>
2023-10-07 06:00:09 +0000

    data types

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "232370024@gift.edu.pk"
+var accountPassword = "12345"
+accountCity ="gujranwala"
+let accountState;
+
+//accountId=2 //not allowed
+accountEmail = "abs@gmail.com"
+accountPassword="212121"
+accountCity = "lhr"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newer version
+//must focus on code readability
+console.log(3+3)
+console.log("Rehman");
+
+let name="RehmanYousaf"
+let age=20
+let isloggedIn= false
+
+//number =>2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false
+//null =>stand alone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
