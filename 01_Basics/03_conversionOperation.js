eb9927951a70661f199dd51535208fc91d7b3703
RehmanYousaf152 <147226228+RehmanYousaf152@users.noreply.github.com>
2023-10-12 06:06:19 +0000

    comparisons

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -21,5 +21,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let num =33
 let strNum = String(num)
-console.log(strNum);
-console.log(typeof strNum);
+//console.log(strNum);
+//console.log(typeof strNum);
+
+//**********************Operations*******************
+ let value = 3
+ let negValue =-value
+ //console.log(negValue);
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*3);
+//  console.log(2**3);
+//  console.log(2/3);
+//  console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Rehman"
+
+let str3 = str1+str2
+// console.log(str3);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+    
+// console.log(+true);// not to use codes
+// console.log(+"");//////////////
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
