9bbf90d2599aa5e372f81b20ed71a4459d6c9b65
RehmanYousaf152 <147226228+RehmanYousaf152@users.noreply.github.com>
2023-10-12 05:29:38 +0000

    conversions

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc "=> NaN
+// true => 1; //false => 0 
+
+let isLoggedIn = "abc"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; // 0 => false
+// "" => false
+// "Rehman" => true
+
+let num =33
+let strNum = String(num)
+console.log(strNum);
+console.log(typeof strNum);
